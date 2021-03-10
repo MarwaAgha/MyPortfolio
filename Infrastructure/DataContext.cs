4e53ab2c9b11dcaf82df093e84635df9443204cc
Marwa <USER@MARWA-EET1A01>
2021-03-10 22:23:52 +0200

    Add migration

---
+++ Infrastructure/DataContext.cs
@@ -0,0 +1,41 @@
+﻿using Core.Entites;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Infrastructure
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Owner>()
+                .Property(x => x.Id)
+                .HasDefaultValueSql("NEWID()");
+
+            modelBuilder.Entity<PortfolioItem>()
+                .Property(x => x.Id)
+                .HasDefaultValueSql("NEWID()");
+
+            modelBuilder.Entity<Owner>().HasData(
+                new Owner()
+                {
+                    Id = Guid.NewGuid(),
+                    Avatar = "Avatar.jpg",
+                    FullName = "Marwa Mahmoud Agha",
+                    Profil = "Web Application Developer"
+                }
+                );
+        }
+
+        public DbSet<Owner> Owner { get; set; }
+        public DbSet<PortfolioItem> PortfolioItems { get; set; }
+
+    }
+}
